9b4db8c7540c195dd31cb97ff6705d164387da22
Doug Wolf <douglaswolf@yahoo.com>
2022-05-06 18:57:26 -0700

    No commit message

--- hls_demo.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
+++ hls_demo.gen/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2021.2 (win64) Build 3367213 Tue Oct 19 02:48:09 MDT 2021
-//Date        : Wed May  4 19:39:33 2022
+//Date        : Fri May  6 18:49:13 2022
 //Host        : DESKTOP-06LMOH5 running 64-bit major release  (build 9200)
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
--- hls_demo.gen/sources_1/bd/design_1/ip/design_1_smartconnect_0_0/bd_0/hdl/bd_48ac_wrapper.v
+++ hls_demo.gen/sources_1/bd/design_1/ip/design_1_smartconnect_0_0/bd_0/hdl/bd_48ac_wrapper.v
@@ -83,6 +83,25 @@ module bd_48ac_wrapper
     S01_AXI_wready,
     S01_AXI_wstrb,
     S01_AXI_wvalid,
+    S02_AXI_araddr,
+    S02_AXI_arprot,
+    S02_AXI_arready,
+    S02_AXI_arvalid,
+    S02_AXI_awaddr,
+    S02_AXI_awprot,
+    S02_AXI_awready,
+    S02_AXI_awvalid,
+    S02_AXI_bready,
+    S02_AXI_bresp,
+    S02_AXI_bvalid,
+    S02_AXI_rdata,
+    S02_AXI_rready,
+    S02_AXI_rresp,
+    S02_AXI_rvalid,
+    S02_AXI_wdata,
+    S02_AXI_wready,
+    S02_AXI_wstrb,
+    S02_AXI_wvalid,
     aclk,
     aresetn);
   output [3:0]M00_AXI_araddr;
@@ -161,6 +180,25 @@ module bd_48ac_wrapper
   output S01_AXI_wready;
   input [3:0]S01_AXI_wstrb;
   input S01_AXI_wvalid;
+  input [31:0]S02_AXI_araddr;
+  input [2:0]S02_AXI_arprot;
+  output S02_AXI_arready;
+  input S02_AXI_arvalid;
+  input [31:0]S02_AXI_awaddr;
+  input [2:0]S02_AXI_awprot;
+  output S02_AXI_awready;
+  input S02_AXI_awvalid;
+  input S02_AXI_bready;
+  output [1:0]S02_AXI_bresp;
+  output S02_AXI_bvalid;
+  output [31:0]S02_AXI_rdata;
+  input S02_AXI_rready;
+  output [1:0]S02_AXI_rresp;
+  output S02_AXI_rvalid;
+  input [31:0]S02_AXI_wdata;
+  output S02_AXI_wready;
+  input [3:0]S02_AXI_wstrb;
+  input S02_AXI_wvalid;
   input aclk;
   input aresetn;
 
@@ -240,6 +278,25 @@ module bd_48ac_wrapper
   wire S01_AXI_wready;
   wire [3:0]S01_AXI_wstrb;
   wire S01_AXI_wvalid;
+  wire [31:0]S02_AXI_araddr;
+  wire [2:0]S02_AXI_arprot;
+  wire S02_AXI_arready;
+  wire S02_AXI_arvalid;
+  wire [31:0]S02_AXI_awaddr;
+  wire [2:0]S02_AXI_awprot;
+  wire S02_AXI_awready;
+  wire S02_AXI_awvalid;
+  wire S02_AXI_bready;
+  wire [1:0]S02_AXI_bresp;
+  wire S02_AXI_bvalid;
+  wire [31:0]S02_AXI_rdata;
+  wire S02_AXI_rready;
+  wire [1:0]S02_AXI_rresp;
+  wire S02_AXI_rvalid;
+  wire [31:0]S02_AXI_wdata;
+  wire S02_AXI_wready;
+  wire [3:0]S02_AXI_wstrb;
+  wire S02_AXI_wvalid;
   wire aclk;
   wire aresetn;
 
@@ -320,6 +377,25 @@ module bd_48ac_wrapper
         .S01_AXI_wready(S01_AXI_wready),
         .S01_AXI_wstrb(S01_AXI_wstrb),
         .S01_AXI_wvalid(S01_AXI_wvalid),
+        .S02_AXI_araddr(S02_AXI_araddr),
+        .S02_AXI_arprot(S02_AXI_arprot),
+        .S02_AXI_arready(S02_AXI_arready),
+        .S02_AXI_arvalid(S02_AXI_arvalid),
+        .S02_AXI_awaddr(S02_AXI_awaddr),
+        .S02_AXI_awprot(S02_AXI_awprot),
+        .S02_AXI_awready(S02_AXI_awready),
+        .S02_AXI_awvalid(S02_AXI_awvalid),
+        .S02_AXI_bready(S02_AXI_bready),
+        .S02_AXI_bresp(S02_AXI_bresp),
+        .S02_AXI_bvalid(S02_AXI_bvalid),
+        .S02_AXI_rdata(S02_AXI_rdata),
+        .S02_AXI_rready(S02_AXI_rready),
+        .S02_AXI_rresp(S02_AXI_rresp),
+        .S02_AXI_rvalid(S02_AXI_rvalid),
+        .S02_AXI_wdata(S02_AXI_wdata),
+        .S02_AXI_wready(S02_AXI_wready),
+        .S02_AXI_wstrb(S02_AXI_wstrb),
+        .S02_AXI_wvalid(S02_AXI_wvalid),
         .aclk(aclk),
         .aresetn(aresetn));
 endmodule
